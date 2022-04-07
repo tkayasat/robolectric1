79544c8e6c54197bd99dc3d549bcd4c77343e118
tkaya <tkayasat@gmail.com>
2022-04-08 01:50:44 +0300

    hw4 _2

--- app/src/test/java/com/example/robolectric/MainActivityEspressoTest.kt
+++ app/src/test/java/com/example/robolectric/MainActivityEspressoTest.kt
@@ -2,13 +2,14 @@ package com.example.robolectric
 
 import android.view.View
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.robolectric.view.search.MainActivity
 import org.hamcrest.Matcher
@@ -28,20 +29,12 @@ class MainActivityEspressoTest {
     }
 
     @Test
-    fun activitySearch_IsWorking() {
-        Espresso.onView(withId(R.id.searchEditText)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.searchEditText))
-            .perform(ViewActions.replaceText("algol"), ViewActions.closeSoftKeyboard())
-        Espresso.onView(withId(R.id.searchEditText)).perform(ViewActions.pressImeActionButton())
-
-        if (BuildConfig.TYPE == MainActivity.FAKE) {
-            Espresso.onView(withId(R.id.totalCountTextView))
-                .check(ViewAssertions.matches(ViewMatchers.withText("Number of results: 42")))
-        } else {
-            Espresso.onView(ViewMatchers.isRoot()).perform(delay())
-            Espresso.onView(withId(R.id.totalCountTextView))
-                .check(ViewAssertions.matches(ViewMatchers.withText("Number of results: 2283")))
-        }
+    fun activitySearch_IsSearchWorking() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(replaceText("algol"),
+            closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
     }
 
 
