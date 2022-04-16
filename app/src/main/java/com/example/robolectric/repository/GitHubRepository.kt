654c01030d29fed3ba041c7e4c4f7b11700abf74
tkaya <tkayasat@gmail.com>
2022-04-16 05:09:54 +0300

    hw7

--- app/src/main/java/com/example/robolectric/repository/GitHubRepository.kt
+++ app/src/main/java/com/example/robolectric/repository/GitHubRepository.kt
@@ -1,10 +1,10 @@
 package com.example.robolectric.repository
 
 import com.example.robolectric.model.SearchResponse
+import com.example.robolectric.model.SearchResult
 import com.example.robolectric.presenter.presenter.RepositoryContract
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
+import kotlin.random.Random
 
 internal class GitHubRepository(private val gitHubApi: GitHubApi) : RepositoryContract {
 
@@ -12,22 +12,29 @@ internal class GitHubRepository(private val gitHubApi: GitHubApi) : RepositoryCo
         query: String,
         callback: RepositoryCallback,
     ) {
-        val call = gitHubApi.searchGithub(query)
-        call?.enqueue(object : Callback<SearchResponse?> {
+        callback.handleGitHubResponse(Response.success(getFakeResponse()))
+    }
 
-            override fun onResponse(
-                call: Call<SearchResponse?>,
-                response: Response<SearchResponse?>,
-            ) {
-                callback.handleGitHubResponse(response)
-            }
-
-            override fun onFailure(
-                call: Call<SearchResponse?>,
-                t: Throwable,
-            ) {
-                callback.handleGitHubError()
-            }
-        })
+    private fun getFakeResponse(): SearchResponse {
+        val list: MutableList<SearchResult> = mutableListOf()
+        for (index in 1..100) {
+            list.add(
+                SearchResult(
+                    id = index,
+                    name = "Name: $index",
+                    fullName = "FullName: $index",
+                    private = Random.nextBoolean(),
+                    description = "Description: $index",
+                    updatedAt = "Updated: $index",
+                    size = index,
+                    stargazersCount = Random.nextInt(100),
+                    language = "",
+                    hasWiki = Random.nextBoolean(),
+                    archived = Random.nextBoolean(),
+                    score = index.toDouble()
+                )
+            )
+        }
+        return SearchResponse(list.size, list)
     }
 }
--- app/src/main/java/com/example/robolectric/view/search/SearchResultAdapter.kt
+++ app/src/main/java/com/example/robolectric/view/search/SearchResultAdapter.kt
@@ -3,18 +3,20 @@ package com.example.robolectric.view.search
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.robolectric.R
 import com.example.robolectric.model.SearchResult
 import kotlinx.android.synthetic.main.list_item.view.*
 
-internal class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.SearchResultViewHolder>() {
+internal class SearchResultAdapter :
+    RecyclerView.Adapter<SearchResultAdapter.SearchResultViewHolder>() {
 
     private var results: List<SearchResult> = listOf()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
-        viewType: Int
+        viewType: Int,
     ): SearchResultViewHolder {
         return SearchResultViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.list_item, null)
@@ -23,7 +25,7 @@ internal class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Se
 
     override fun onBindViewHolder(
         holder: SearchResultViewHolder,
-        position: Int
+        position: Int,
     ) {
         holder.bind(results[position])
     }
@@ -41,6 +43,9 @@ internal class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Se
 
         fun bind(searchResult: SearchResult) {
             itemView.repositoryName.text = searchResult.fullName
+            itemView.repositoryName.setOnClickListener {
+                Toast.makeText(itemView.context, searchResult.fullName, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
